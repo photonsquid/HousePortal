e1b7804bb423c8693e9541953bc9d7812e3f3502
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2022-06-01 00:20:31 +0200

    test: :test_tube: solve jest mocking issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,16 +6,15 @@ import App from 'App';
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
     writable: true,
-    value: jest.fn().mockImplementation((query) => ({
+    value: () => ({
       matches: false,
-      media: query,
       onchange: null,
       addListener: jest.fn(), // deprecated
       removeListener: jest.fn(), // deprecated
       addEventListener: jest.fn(),
       removeEventListener: jest.fn(),
       dispatchEvent: jest.fn(),
-    })),
+    }),
   });
 });
 
