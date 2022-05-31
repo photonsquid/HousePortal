b5a97bbd2e4684b43cdfafd05e8e8a79f0639d6a
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2022-06-01 00:11:22 +0200

    test: :test_tube: hopefully fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,22 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from 'App';
 
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
 test('renders app correctly', () => {
   render(<App />);
   const linkElement = screen.getByText(/Logging in/i);
