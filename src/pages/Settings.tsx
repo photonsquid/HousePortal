45bf553c2ed3666bb9dff9bafab670b4c7c8a0b0
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2022-06-01 02:20:19 +0200

    fix: :ambulance: prevent submit button dangerous default behavior

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,7 +5,8 @@ import { useParams } from 'react-router-dom';
 import { BiCog, BiLock } from 'react-icons/bi';
 import { MdOutlineAccountCircle } from 'react-icons/md';
 import { IoBrushOutline } from 'react-icons/io5';
-import Form, { FormElementType, JsonCategory } from 'components/forms/Form';
+import Form, { FieldType, JsonCategory } from 'components/forms/Form';
+import RemoteStorage from 'utils/RemoteStorage';
 
 export class SettingsBody {
   static jsonContent: JsonCategory[] = [
@@ -16,17 +17,33 @@ export class SettingsBody {
           name: 'General',
           url: 'general',
           icon: <BiCog size="1.3em" />,
-          fields: {},
+          fields: [
+            {
+              name: 'Size of your dick',
+              id: 'dick-size',
+              type: FieldType.Select,
+              default: 'small',
+              options: {
+                small: 'Small',
+                medium: 'Medium',
+                large: 'Large',
+                enormous: 'Enormous',
+              },
+              editable: true,
+              description: 'The size of your dick',
+            },
+          ],
         },
         {
           name: 'Appearance',
           url: 'appearance',
           icon: <IoBrushOutline size="1.3em" />,
-          fields: {
-            themeSelect: {
+          fields: [
+            {
               name: 'Theme',
-              type: FormElementType.Select,
-              value: 'Light',
+              id: 'theme-select',
+              type: FieldType.Select,
+              default: 'light',
               options: {
                 light: 'Light',
                 system: 'System',
@@ -35,10 +52,11 @@ export class SettingsBody {
               editable: true,
               description: 'The theme of the application',
             },
-            themeRadio: {
+            {
               name: 'Theme',
-              type: FormElementType.Radio,
-              value: 'Light',
+              id: 'theme-radiobutton',
+              type: FieldType.Radio,
+              default: 'light',
               options: {
                 light: 'Light',
                 system: 'System',
@@ -48,19 +66,19 @@ export class SettingsBody {
               description: 'The theme of the application',
 
             },
-          },
+          ],
         },
         {
           name: 'Authentication',
           url: 'authentication',
           icon: <BiLock size="1.3em" />,
-          fields: {},
+          fields: [],
         },
         {
           name: 'Accounts',
           url: 'accounts',
           icon: <MdOutlineAccountCircle size="1.3em" />,
-          fields: {},
+          fields: [],
         },
 
       ],
@@ -72,13 +90,13 @@ export class SettingsBody {
           name: 'General',
           url: 'remote-general',
           icon: <BiCog size="1.3em" />,
-          fields: {},
+          fields: [],
         },
         {
           name: 'Appearance',
           url: 'remote-appearance',
           icon: <IoBrushOutline size="1.3em" />,
-          fields: {},
+          fields: [],
         },
       ],
     },
@@ -91,7 +109,14 @@ export class SettingsBody {
         name: tab.name,
         url: tab.url,
         icon: tab.icon,
-        content: <Form title={tab.name} formData={tab.fields} />,
+        content: <Form
+          title={tab.name}
+          formData={tab.fields}
+          fieldActions={{
+            get: (id: string) => RemoteStorage.getUserSetting(id),
+            set: (id: string, value: string) => RemoteStorage.setUserSetting(id, value),
+          }}
+        />,
       })),
     } as TabCategory));
   }
